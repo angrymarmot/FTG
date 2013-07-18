9b7b8270aa41f724fb1e472e1158d53febc29765
AngryMarmot <angrymarmot@fastmail.net>
2013-07-18 16:21:06 -0700

    0.1

--- ftg.py
+++ ftg.py
@@ -1,20 +1,39 @@
 __author__ = 'angrymarmot'
-
 #Approach.
 
-# Randomly generate E, V I events first.
-# Randomly generate E, V O events where O > I but less than 1440 for the time value
+#Randomly generate E, V I events first.
+#Randomly generate E, V O events where O > I but less than 1440 for the time value
 
-# events = [[]]
-# For r in xrange (e/2):
-   # t = rand(0 to 1440)
-  #  r = rand(0 to 100)
-   # events.append([p, r, I, t])
-   # t = rand(t to 1440) (so it's greater than original t)
-   # events.append([p, r, O, t])
-#
-#
+#eventcount, personlimit, roomlimit, ee, le = argv0, argv1, argv2, argv3, argv4
+#events = [[]]
+#roomlist = []
+#personlist = []
+#For r in xrange (eventcount/2):
+#    t = rand(ee to 1440)
+#    r = rand(0 to roomlimit)
+#    p = rand(0 to personlimit)
+#    personlist.append(p)
+#    roomlist.append(r)
+#    events.append([p, r, I, t])
+#    t = rand(t to le) (so it's greater than original t)
+#    events.append([p, r, O, t])
+
 from sys import argv
+import random
 
+eventcount, personlimit, roomlimit, ee, le = int(argv[1]), int(argv[2]), int(argv[3]), int(argv[4]), int(argv[5])
 events = [[]]
+roomlist = []
+personlist = [x for x in xrange(personlimit)]
 
+for r in xrange(eventcount / 2):
+    random.seed()
+    t = random.randrange(ee, le, 1)
+    r = random.randrange(0, roomlimit,1)
+    p = sorted(personlist.pop())
+    events.append([p, r, 'I', t])
+    t = random.randrange(t, le, 1)
+    events.append([p, r, 'O', t])
+
+for item in events:
+    print item
