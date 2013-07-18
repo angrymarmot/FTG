8c45f250deb3e80cf2cddd6163ec6d08fa414f31
AngryMarmot <angrymarmot@fastmail.net>
2013-07-18 16:07:31 -0700

    update

--- ftg.py
+++ ftg.py
@@ -1,4 +1,20 @@
 __author__ = 'angrymarmot'
 
+#Approach.
 
+# Randomly generate E, V I events first.
+# Randomly generate E, V O events where O > I but less than 1440 for the time value
+
+# events = [[]]
+# For r in xrange (e/2):
+   # t = rand(0 to 1440)
+  #  r = rand(0 to 100)
+   # events.append([p, r, I, t])
+   # t = rand(t to 1440) (so it's greater than original t)
+   # events.append([p, r, O, t])
+#
+#
+from sys import argv
+
+events = [[]]
 
