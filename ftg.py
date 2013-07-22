19d696ceaf0d1b1550f42b4a5fcc091d47a8d5dd
angrymarmot <angrymarmot@fastmail.net>
2013-07-21 19:20:00 -0700

    final

--- ftg.py
+++ ftg.py
@@ -23,19 +23,22 @@ import random
 
 eventcount, personlimit, roomlimit, ee, le = int(argv[1]), int(argv[2]), int(argv[3]), int(argv[4]), int(argv[5])
 events = [[]]
-roomlist = []
 personlist = [x for x in xrange(personlimit)]
 personlist = sorted(personlist)
-modp = eventcount / len(personlist)
-print modp
-for r in xrange(eventcount / 2):
-    random.seed()
+random.seed()
+p = 0
+total = 0
+for r in xrange(eventcount):
     t = random.randrange(ee, le, 1)
-    r = random.randrange(0, roomlimit,1)
-    p = personlist[0]
+    r = random.randrange(0, roomlimit, 1)
     events.append([p, r, 'I', t])
+    total += 1
     t = random.randrange(t, le, 1)
     events.append([p, r, 'O', t])
-
+    total += 1
+    p += 1
+    if p == (personlimit + 1):
+        p = 1
+events[0] = [total]
 for item in events:
-    print item
+    print str(item).strip('[]')
